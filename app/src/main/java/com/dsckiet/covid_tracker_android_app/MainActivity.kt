eae819c6495da372454fdfede9e9c7f75a847dcc
Anshul1507 <ag1507anshul@gmail.com>
2020-06-19 11:29:45 +0530

    added dependencies

---
+++ app/src/main/java/com/dsckiet/covid_tracker_android_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dsckiet.covid_tracker_android_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
