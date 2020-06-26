8954c19efb4c3c9cb4d05148f240444202ff1c65
jhavidit <54817177+jhavidit@users.noreply.github.com>
2020-06-26 20:15:32 +0530

    Time Conversion Util

---
+++ app/src/main/java/com/dsckiet/covid_tracker_android_app/utils/TimeUtils.kt
@@ -0,0 +1,42 @@
+package com.dsckiet.covid_tracker_android_app.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+fun getPeriod(past: Date): String {
+    val now = Date()
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+    val hours = TimeUnit.MILLISECONDS.toHours(now.time - past.time)
+
+    return when {
+        seconds < 60 -> {
+            "Recently"
+        }
+        minutes < 60 -> {
+            "$minutes minutes ago"
+        }
+
+        hours < 24 -> {
+            "$hours hour ${minutes % 60} min ago"
+        }
+        else -> {
+            SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
+        }
+    }
+}
+
+fun String.toDateFormat(): Date {
+    return SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        .parse(this)
+}
+
+fun globalTimeDateFormat(time: String): Date {
+    val format = SimpleDateFormat(
+        "yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US
+    )
+    format.timeZone = TimeZone.getTimeZone("UTC")
+    return format.parse(time)
+
+}
