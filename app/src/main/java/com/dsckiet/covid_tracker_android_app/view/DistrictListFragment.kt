c59123106882326f6b702740c39583bb12daedda
jhavidit <54817177+jhavidit@users.noreply.github.com>
2020-07-05 23:14:54 +0530

    District List Fragment Class.

---
+++ app/src/main/java/com/dsckiet/covid_tracker_android_app/view/DistrictListFragment.kt
@@ -0,0 +1,68 @@
+package com.dsckiet.covid_tracker_android_app.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.dsckiet.covid_tracker_android_app.R
+import com.dsckiet.covid_tracker_android_app.adapter.DistrictAdapter
+import com.dsckiet.covid_tracker_android_app.viewModel.DistrictWiseTrackerViewModel
+import kotlinx.android.synthetic.main.fragment_country_list.back_button
+import kotlinx.android.synthetic.main.fragment_district_list.*
+
+
+class DistrictListFragment : Fragment() {
+    lateinit var navController: NavController
+    lateinit var viewModel: DistrictWiseTrackerViewModel
+    lateinit var adapter: DistrictAdapter
+    var stateCode: String? = null
+    var lastUpdatedTime: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_district_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        back_button.setOnClickListener {
+            navController.popBackStack()
+        }
+        stateCode = arguments?.getString("stateCode")
+        lastUpdatedTime = arguments?.getString("lastUpdatedTime")
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DistrictWiseTrackerViewModel::class.java)
+        viewModel.getCoronaDistrictDetails()
+        adapter = DistrictAdapter(requireContext())
+
+        recycler_view_district_list.adapter = adapter
+        if (stateCode != null) {
+            viewModel.showDistrictWiseDetails.observe(viewLifecycleOwner, Observer {
+                for (i in it.indices)
+                    if (it[i].statecode == stateCode) {
+                        if (lastUpdatedTime != null)
+                            last_updated.text = lastUpdatedTime
+                        state_name.text = it[i].state
+                        adapter.setDistrictWiseTracker(it[i].districtData)
+                    }
+            })
+        }
+
+    }
+
+
+}
