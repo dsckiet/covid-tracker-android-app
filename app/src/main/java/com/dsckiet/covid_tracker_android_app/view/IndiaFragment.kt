2db12d01bdb5a0ebd3c4e3244841f2f66498a7f4
jhavidit <54817177+jhavidit@users.noreply.github.com>
2020-07-14 13:18:54 +0530

    India Fragment.

--- app/src/main/java/com/dsckiet/covid_tracker_android_app/view/IndiaFragment.kt
+++ app/src/main/java/com/dsckiet/covid_tracker_android_app/view/IndiaFragment.kt
@@ -77,86 +77,94 @@ class IndiaFragment : Fragment() {
         viewModel.getCoronaStateDetails()
         recycler_view.adapter = adapter
 
-        viewModel.showCoronaIndiaLineChart.observe(viewLifecycleOwner, Observer {
-            current_date.text = it[it.size - 1].date
-            confirmed_case.text = stringToNumberFormat(it[it.size - 1].totalconfirmed)
-            confirmed_case_delta.text = "+" + stringToNumberFormat(it[it.size - 1].dailyconfirmed)
-            for (i in 30 until it.size) {
-                if (i % 2 == 0)
-                    entries.add(
-                        Entry(
-                            (i + 1 - 30).toFloat(),
-                            (it[i].totalconfirmed).toFloat()
-                        )
-                    )
-                xAxisLabel.add(it[i].date.substring(0, 6))
-            }
 
-            val vl = LineDataSet(entries, "Corona Positive")
-            vl.setDrawCircleHole(true)
-            vl.setDrawValues(false)
-            vl.setDrawFilled(true)
-
-            vl.color = Color.parseColor("#FFE0E6")
-            vl.fillColor = Color.parseColor("#FFE0E6")
-            vl.fillAlpha = Color.RED
-            vl.setCircleColor(Color.parseColor("#FF063A"))
-            vl.circleHoleColor = Color.parseColor("#FF063A")
-            line_chart.description.text = ""
-            line_chart.legend.isEnabled = false
-            line_chart.axisRight.isEnabled = true
-            line_chart.axisRight.setDrawGridLines(false)
-            line_chart.axisRight.valueFormatter = LargeValueFormatter()
-            line_chart.xAxis.setDrawGridLines(false)
-
-            line_chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-            line_chart.axisLeft.isEnabled = false
-            line_chart.axisRight.gridColor =
-                (Color.parseColor("#FF063A"))
-            line_chart.xAxis.gridColor =
-                (Color.parseColor("#FF063A"))
-            line_chart.xAxis.axisLineColor = (Color.parseColor("#FF063A"))
-            line_chart.axisRight.axisLineColor = (Color.parseColor("#FF063A"))
-
-            vl.setDrawCircles(true)
-
-            line_chart.setDrawMarkers(true)
-            line_chart.animateY(2000)
-            line_chart.setDrawGridBackground(false)
-            line_chart.isHighlightPerDragEnabled = true
-
-            line_chart.data = LineData(vl)
-            line_chart.xAxis.valueFormatter = IndexAxisValueFormatter(xAxisLabel)
-            line_chart.xAxis.setLabelCount(20, false)
-            line_chart.xAxis.textSize = 6f
-            line_chart.xAxis.textColor =
-                (Color.parseColor("#E86985"))
-            line_chart.axisRight.textColor =
-                (Color.parseColor("#E86985"))
-
-
-            line_chart.setTouchEnabled(false)
-            line_chart.setPinchZoom(false)
-
-
-        })
-        viewModel.showProgress.observe(viewLifecycleOwner, Observer {
-            swipeRefreshLayout.isRefreshing = it
-        })
 
 
         viewModel.showCoronaStateDetails.observe(viewLifecycleOwner, Observer {
-            val lastUpdatedTime =
-                "Last Updated " + getPeriod(it[0].lastupdatedtime.toDateFormat()!!)
-            last_updated.text = lastUpdatedTime
-            confirmed_count.text = stringToNumberFormat(it[0].confirmed)
-            recover_count.text = stringToNumberFormat(it[0].recovered)
-            rip_count.text = stringToNumberFormat(it[0].deaths)
-            active_count.text = stringToNumberFormat(it[0].active)
-            confirmed_arrow_number.text = stringToNumberFormat(it[0].deltarecovered)
-            recover_arrow_number.text = stringToNumberFormat(it[0].deltarecovered)
-            rip_arrow_number.text = stringToNumberFormat(it[0].deltadeaths)
-            adapter.setStateWiseTracker(it)
+            if (!swipeRefreshLayout.isRefreshing) {
+                val lastUpdatedTime =
+                    "Last Updated " + getPeriod(it[0].lastupdatedtime.toDateFormat()!!)
+                last_updated.text = lastUpdatedTime
+                confirmed_count.text = stringToNumberFormat(it[0].confirmed)
+                recover_count.text = stringToNumberFormat(it[0].recovered)
+                rip_count.text = stringToNumberFormat(it[0].deaths)
+                active_count.text = stringToNumberFormat(it[0].active)
+                confirmed_arrow_number.text = stringToNumberFormat(it[0].deltarecovered)
+                recover_arrow_number.text = stringToNumberFormat(it[0].deltarecovered)
+                rip_arrow_number.text = stringToNumberFormat(it[0].deltadeaths)
+                adapter.setStateWiseTracker(it)
+            }
+        })
+        viewModel.showCoronaIndiaLineChart.observe(viewLifecycleOwner, Observer {
+            if (!swipeRefreshLayout.isRefreshing) {
+                entries.clear()
+                xAxisLabel.clear()
+                current_date.text = it[it.size - 1].date
+                confirmed_case.text = stringToNumberFormat(it[it.size - 1].totalconfirmed)
+                confirmed_case_delta.text =
+                    "+" + stringToNumberFormat(it[it.size - 1].dailyconfirmed)
+                for (i in 30 until it.size) {
+                    if (i % 2 == 0)
+                        entries.add(
+                            Entry(
+                                (i + 1 - 30).toFloat(),
+                                (it[i].totalconfirmed).toFloat()
+                            )
+                        )
+                    xAxisLabel.add(it[i].date.substring(0, 6))
+                }
+
+                val vl = LineDataSet(entries, "Corona Positive")
+                vl.setDrawCircleHole(true)
+                vl.setDrawValues(false)
+                vl.setDrawFilled(true)
+
+                vl.color = Color.parseColor("#FFE0E6")
+                vl.fillColor = Color.parseColor("#FFE0E6")
+                vl.fillAlpha = Color.RED
+                vl.setCircleColor(Color.parseColor("#FF063A"))
+                vl.circleHoleColor = Color.parseColor("#FF063A")
+                line_chart.description.text = ""
+                line_chart.legend.isEnabled = false
+                line_chart.axisRight.isEnabled = true
+                line_chart.axisRight.setDrawGridLines(false)
+                line_chart.axisRight.valueFormatter = LargeValueFormatter()
+                line_chart.xAxis.setDrawGridLines(false)
+
+                line_chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+                line_chart.axisLeft.isEnabled = false
+                line_chart.axisRight.gridColor =
+                    (Color.parseColor("#FF063A"))
+                line_chart.xAxis.gridColor =
+                    (Color.parseColor("#FF063A"))
+                line_chart.xAxis.axisLineColor = (Color.parseColor("#FF063A"))
+                line_chart.axisRight.axisLineColor = (Color.parseColor("#FF063A"))
+
+                vl.setDrawCircles(true)
+
+                line_chart.setDrawMarkers(true)
+                line_chart.animateX(2000)
+                line_chart.setDrawGridBackground(false)
+                line_chart.isHighlightPerDragEnabled = true
+
+                line_chart.data = LineData(vl)
+                line_chart.xAxis.valueFormatter = IndexAxisValueFormatter(xAxisLabel)
+                line_chart.xAxis.setLabelCount(20, false)
+                line_chart.xAxis.textSize = 6f
+                line_chart.xAxis.textColor =
+                    (Color.parseColor("#E86985"))
+                line_chart.axisRight.textColor =
+                    (Color.parseColor("#E86985"))
+
+
+                line_chart.setTouchEnabled(false)
+                line_chart.setPinchZoom(false)
+
+            }
+        })
+
+        viewModel.showProgress.observe(viewLifecycleOwner, Observer {
+            swipeRefreshLayout.isRefreshing = it
         })
     }
 }
