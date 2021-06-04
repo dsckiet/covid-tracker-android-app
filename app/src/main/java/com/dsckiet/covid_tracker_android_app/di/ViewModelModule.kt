dd3ca029bdbdd760a51975a13b9ec38854e50d79
Aditya <icgadi99@gmail.com>
2021-06-04 14:39:02 +0530

    Bind ViewModels into ViewModel Module

--- app/src/main/java/com/dsckiet/covid_tracker_android_app/di/ViewModelModule.kt
+++ app/src/main/java/com/dsckiet/covid_tracker_android_app/di/ViewModelModule.kt
@@ -1,14 +1,27 @@
 package com.dsckiet.covid_tracker_android_app.di
 
 import androidx.lifecycle.ViewModel
+import com.dsckiet.covid_tracker_android_app.ui.global.CountryWiseTrackerViewModel
+import com.dsckiet.covid_tracker_android_app.ui.india.DistrictWiseTrackerViewModel
+import com.dsckiet.covid_tracker_android_app.ui.india.StateWiseTrackerViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
-//    @Binds
-//    @IntoMap
-//    @ViewModelKey(MainViewModel::class)
-//    abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
+    @Binds
+    @IntoMap
+    @ViewModelKey(CountryWiseTrackerViewModel::class)
+    abstract fun bindCountryWiseTrackerViewModel(countryWiseTrackerViewModel: CountryWiseTrackerViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DistrictWiseTrackerViewModel::class)
+    abstract fun bindDistrictWiseTrackerViewModel(districtWiseTrackerViewModel: DistrictWiseTrackerViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(StateWiseTrackerViewModel::class)
+    abstract fun bindStateWiseTrackerViewModel(stateWiseTrackerViewModel: StateWiseTrackerViewModel): ViewModel
 }
